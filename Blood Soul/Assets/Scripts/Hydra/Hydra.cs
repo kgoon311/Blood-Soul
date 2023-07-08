7b61e384af84d4108988dbd9c9f176a9496aa8b9
kgoon311 <ehdwls4938@naver.com>
2023-07-09 05:24:55 +0900

    Fix Breath Effect & sword Effect

--- Blood Soul/Assets/Scripts/Hydra/Hydra.cs
+++ Blood Soul/Assets/Scripts/Hydra/Hydra.cs
@@ -1,19 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
 enum Pattern
 {
-    SingleBreath,
-    DoubleBreath,
-    TripleBreath,
+    Breath,
+    Earthquake,
     Bite,
     TailAttack,
-    Earthquake,
-    PosionZone
-
 }
 public class Hydra : MonoBehaviour
 {
@@ -29,14 +26,15 @@ public class Hydra : MonoBehaviour
     private bool isMove = true;
     [SerializeField] private int phase = 0;
     [Header("Status")]
+    [SerializeField] private float maxHp;
+    [SerializeField] private float hp;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float[] attackSpeed;
+    private float timer;
 
     [Header("Attack")]
-    [SerializeField] private int attackPatternRange;
-    [SerializeField] private List<GameObject> attackCollider = new List<GameObject>();
-    private int beforeParttern;
+    private int beforeParttern = -1;
 
     [Header("Bite")]
     [SerializeField] private float bite_AngleRange = 30f;
@@ -52,26 +50,44 @@ public class Hydra : MonoBehaviour
     private bool isEarthQuakeHit;
     private bool isLeftFoot = false;
 
-    [Header("Laser")]
-    [SerializeField] private GameObject[] laserPS;
+    [Header("Breath")]
+    [SerializeField] private float breathPatternRange;
+    [SerializeField] private GameObject[] breathPS;
     [SerializeField] private GameObject[] chargerPS;
+    [SerializeField] private GameObject[] headPos;
     private void Awake()
     {
         instance = this;
+
         myAnimator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody>();
+
+        hp = maxHp;
+        timer = Random.Range(attackSpeed[0], attackSpeed[1]);
     }
 
     [SerializeField] private float MoveRange;
     private void Update()
     {
         if (isMove == true)
-            Move();
-        if (Input.GetKeyDown(KeyCode.Q))
         {
-            AttackAnim(testType);
-            //StartCoroutine(TailAttack());
+            Move();
+            AttackTimer();
         }
+        if (isTestAttack &&Input.GetKeyDown(KeyCode.Q))
+        {
+            PlayAttackAnim(testType);
+        }
+
+        float percent = hp / maxHp;
+        
+        if (percent < 0.6f)
+        {
+            phase = 1;
+        }
+        else if(percent < 0.3f)
+            phase = 2;
+
     }
     private void Move()
     {
@@ -94,59 +110,77 @@ public class Hydra : MonoBehaviour
             myRigidbody.velocity = Vector3.zero;
         }
     }
-    private void AttackAnim(Pattern pattern)
+    private void AttackTimer()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            float dis = Vector3.Distance(transform.position, player.transform.position);
+
+            if (dis > breathPatternRange)
+            {
+                if (phase == 0)
+                    beforeParttern = 0;
+                else
+                    beforeParttern = Random.Range(0, 2);
+
+            }
+            else
+            { 
+                    beforeParttern = Random.Range(2, 4);
+            }
+
+            PlayAttackAnim((Pattern)beforeParttern);
+            myRigidbody.velocity = Vector3.zero;
+            timer = Random.Range(attackSpeed[0], attackSpeed[1]);
+        }
+    }
+
+    private void PlayAttackAnim(Pattern pattern)
     {
         switch (pattern)
         {
-            case Pattern.SingleBreath:
+            case Pattern.Breath:
                 {
-                    myAnimator.SetTrigger("Single");
-                    break;
-                }
-            case Pattern.DoubleBreath:
-                {
-                    myAnimator.SetTrigger("Double");
-                    break;
-                }
-            case Pattern.TripleBreath:
-                {
-                    myAnimator.SetTrigger("Triple");
+                    if(phase == 0)
+                        myAnimator.SetTrigger("Single");
+                    else if(phase == 1)
+                        myAnimator.SetTrigger("Double");
+                    else if(phase == 2)
+                        myAnimator.SetTrigger("Triple");
+
+                    StartCoroutine(BreathAttack());
                     break;
                 }
             case Pattern.Bite:
                 {
                     myAnimator.SetTrigger("Bite");
+                    StartCoroutine(BiteAttack());
                     break;
                 }
             case Pattern.TailAttack:
                 {
                     myAnimator.SetTrigger("TaillAttack");
+                    StartCoroutine(TailAttack());
                     break;
                 }
             case Pattern.Earthquake:
                 {
                     myAnimator.SetTrigger("EarthQuake");
-                    break;
-                }
-            case Pattern.PosionZone:
-                {
-                    myAnimator.SetTrigger("Posion");
+                    StartCoroutine(EarthQuake());
                     break;
                 }
         }
     }
 
+    ///////////////// - Attack Pattern - //////////////////////
     private IEnumerator BiteAttack()
     {
         isMove = false;
 
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.7f);
 
-        AttackAnim(Pattern.Bite);
-
-        yield return new WaitForSeconds(0.6f);
-
-        bool hit = CircularSector(bite_Radius, bite_AngleRange);
+        bool hit = BiteCircularSector(bite_Radius, bite_AngleRange);
         if (hit)
             Debug.Log("Bite Hit");
 
@@ -154,14 +188,31 @@ public class Hydra : MonoBehaviour
 
         isMove = true;
     }
+    private bool BiteCircularSector(float radius, float range)
+    {
+        Vector3 interV = player.transform.position - transform.position;
+
+        if (interV.magnitude <= radius)
+        {
+            //플레이어와 히드라의 각도를 구한다
+            float dot = Vector3.Dot(interV.normalized, transform.forward);
+            float theta = Mathf.Acos(dot);
+            float degree = Mathf.Rad2Deg * theta;
+
+            if (degree <= range / 2f)
+                return true;
+            else
+                return false;
+
+        }
+        else
+            return false;
+    }
 
     private IEnumerator TailAttack()
     {
         isMove = false;
 
-        yield return new WaitForSeconds(0.1f);
-
-        AttackAnim(Pattern.TailAttack);
         for (int i = 0; i < tailColider.Length; i++)
             tailColider[i].enabled = true;
 
@@ -185,7 +236,6 @@ public class Hydra : MonoBehaviour
     private IEnumerator EarthQuake()
     {
         isMove = false;
-        AttackAnim(Pattern.Earthquake);
 
         yield return new WaitForSeconds(8.2f);
 
@@ -200,20 +250,29 @@ public class Hydra : MonoBehaviour
             Debug.Log("EarthQuake Hit");
         }
     }
-
-
-    private IEnumerator LaserAttack()
+    private IEnumerator BreathAttack()
     {
-        AttackAnim((Pattern)phase);
+        isMove = false;
 
+        yield return new WaitForSeconds(1.5f);
 
-        yield return new WaitForSeconds(3f);
+        float timer = 0;
+        while (timer < 1)
+        {
+            timer += Time.deltaTime / 3;
+            Vector3 dir = headPos[0].transform.position - headPos[1].transform.position;
+            if (Physics.Raycast(headPos[0].transform.position, dir * 20, 100, LayerMask.GetMask("Player")) == true)
+            {
+                Debug.Log("hit");
+            }
+            yield return null;
+        }
 
-
+        isMove = true;
         yield return null;
     }
 
-    ///////////////// - Anim Trigger - ///////////////////
+    ///////////////// - Anim Trigger - //////////////////////
     public void EarthQuakeParticle()
     {
         Instantiate(earthQuakePS, isLeftFoot ? footPos[0].transform.position : footPos[1].transform.position, Quaternion.Euler(90, 0, 0));
@@ -221,26 +280,26 @@ public class Hydra : MonoBehaviour
     }
     public void LaserParticle()
     {
-        StartCoroutine(LaserParticlePlay(phase));
+        StartCoroutine(BreathParticlePlay(phase));
     }
-    private IEnumerator LaserParticlePlay(int idx)
+    private IEnumerator BreathParticlePlay(int idx)
     {
         switch (idx)
         {
             case 0:
                 {
-                    chargerPS[1].SetActive(true);
-                    yield return new WaitForSeconds(1f);
-                    laserPS[1].SetActive(true);
+                    chargerPS[1].SetActive(true); 
+                    yield return new WaitForSeconds(1.5f);
+                    breathPS[1].SetActive(true);
                     break;
                 }
             case 1:
                 {
                     chargerPS[0].SetActive(true);
                     chargerPS[2].SetActive(true);
-                    yield return new WaitForSeconds(1f);
-                    laserPS[0].SetActive(true);
-                    laserPS[2].SetActive(true);
+                    yield return new WaitForSeconds(1.5f);
+                    breathPS[0].SetActive(true);
+                    breathPS[2].SetActive(true);
                     break;
                 }
             case 2:
@@ -248,28 +307,28 @@ public class Hydra : MonoBehaviour
                     chargerPS[0].SetActive(true);
                     chargerPS[1].SetActive(true);
                     chargerPS[2].SetActive(true);
-                    yield return new WaitForSeconds(1f);
-                    laserPS[0].SetActive(true);
-                    laserPS[1].SetActive(true);
-                    laserPS[2].SetActive(true);
+                    yield return new WaitForSeconds(1.5f);
+                    breathPS[0].SetActive(true);
+                    breathPS[1].SetActive(true);
+                    breathPS[2].SetActive(true);
                     break;
                 }
         }
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(3.5f);
         switch (idx)
         {
             case 0:
                 {
                     chargerPS[1].SetActive(false);
-                    laserPS[1].SetActive(false);
+                    breathPS[1].SetActive(false);
                     break;
                 }
             case 1:
                 {
                     chargerPS[0].SetActive(false);
                     chargerPS[2].SetActive(false);
-                    laserPS[0].SetActive(false);
-                    laserPS[2].SetActive(false);
+                    breathPS[0].SetActive(false);
+                    breathPS[2].SetActive(false);
                     break;
                 }
             case 2:
@@ -277,37 +336,18 @@ public class Hydra : MonoBehaviour
                     chargerPS[0].SetActive(false);
                     chargerPS[1].SetActive(false);
                     chargerPS[2].SetActive(false);
-                    laserPS[0].SetActive(false);
-                    laserPS[1].SetActive(false);
-                    laserPS[2].SetActive(false);
+                    breathPS[0].SetActive(false);
+                    breathPS[1].SetActive(false);
+                    breathPS[2].SetActive(false);
                     break;
                 }
         }
     }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
             TailCollision();
-    }
-
-    private bool CircularSector(float radius, float range)
-    {
-        Vector3 interV = player.transform.position - transform.position;
-
-        if (interV.magnitude <= radius)
-        {
-            //플레이어와 히드라의 각도를 구한다
-            float dot = Vector3.Dot(interV.normalized, transform.forward);
-            float theta = Mathf.Acos(dot);
-            float degree = Mathf.Rad2Deg * theta;
-
-            if (degree <= range / 2f)
-                return true;
-            else
-                return false;
-
-        }
-        else
-            return false;
     }
 }
